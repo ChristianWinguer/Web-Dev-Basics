b4083ac2a2cf4a8e1bbdbf2cbbec0bd0aba16020
ChristianWinguer <christianwinguercontato@gmail.com>
2025-01-15 22:30:16 -0300

    Exercicio Dados

--- gerador-numero-aleatorio/index.js
+++ gerador-numero-aleatorio/index.js
@@ -1,31 +1,25 @@
+const inputNumeroDadosJogados = document.querySelector("#inputNumeroDadosJogados");
+const btnJogarDados = document.querySelector("#btnJogarDados");
+const outputMessagem = document.querySelector("#outputMessagem");
 
-const btnRoll = document.getElementById("btnRoll");
-const rolled = document.getElementById("rolled");
-const d4 = document.getElementById("d4");
-const d6 = document.getElementById("d6");
-const d8 = document.getElementById("d8");
-const d10 = document.getElementById("d10");
-const d20 = document.getElementById("d20");
+function jogarDados(numeroDadosJogados) {
+    
+    let resultadoDadosJogados = [];
 
-btnRoll.onclick = function() {
+    for (let i = 0; i < numeroDadosJogados; i++) {
 
-    if (d4.checked) {
-        let randomNumber = Math.floor(Math.random() * 4) + 1;
-        rolled.textContent = `${randomNumber}`
-    } else if (d6.checked) {
-        let randomNumber = Math.floor(Math.random() * 6) + 1;
-        rolled.textContent = `${randomNumber}`
-    } else if (d8.checked) {
-        let randomNumber = Math.floor(Math.random() * 8) + 1;
-        rolled.textContent = `${randomNumber}`
-    } else if (d10.checked) {
-        let randomNumber = Math.floor(Math.random() * 10) + 1;
-        rolled.textContent = `${randomNumber}`
-    } else if (d20.checked) {
-        let randomNumber = Math.floor(Math.random() * 20) + 1;
-        rolled.textContent = `${randomNumber}`
-    } else {
-        rolled.textContent = `Choose one of the dices`
+        let dadoJogado = Math.floor(Math.random() * 6) + 1;
+        resultadoDadosJogados.push(` ${dadoJogado}`);
     }
 
+    return resultadoDadosJogados;
 }
+
+
+btnJogarDados.addEventListener("click", function(event){
+    
+    let numeroDadosJogados = inputNumeroDadosJogados.value;
+    let resultadoDadosJogados = jogarDados(numeroDadosJogados);
+
+    outputMessagem.textContent = `Os resultados dos dados rolados foram ${resultadoDadosJogados}`
+});
