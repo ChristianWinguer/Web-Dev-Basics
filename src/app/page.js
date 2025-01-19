f8a30f5858b0e27551d37f0b256e42c2fe1e59e7
ChristianWinguer <christianwinguercontato@gmail.com>
2025-01-19 00:11:47 -0300

    homePage - +AboutProjectSection e +techStackSection

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,28 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <section className={styles.aboutProjectSection}>
+          <h1>About the project</h1>
+          <p>
+            This project is a responsive and modern website that I developed as
+            a Computer Science student passionate about web development. My goal
+            with this project is to consolidate my knowledge in building web
+            applications using Next.js, while also strengthening my foundation
+            in programming logic and algorithms. This hands-on experience allows
+            me to apply key concepts and best practices in modern web
+            development, preparing me for future challenges in the field.
+          </p>
+      </section>
+      <section className={styles.techStackSection}>
+      <img src="/icons/html-icon.svg" alt="HTML Icon" width={90} height={90} />
+      <img src="/icons/css-icon.svg" alt="HTML Icon" width={90} height={90} />
+      <img src="/icons/javascript-icon.svg" alt="HTML Icon" width={90} height={90} />
+      <img src="/icons/git-icon.svg" alt="HTML Icon" width={90} height={90} />
+      <img src="/icons/nextJS-icon.svg" alt="HTML Icon" width={90} height={90} />
+      </section>
     </div>
   );
 }
